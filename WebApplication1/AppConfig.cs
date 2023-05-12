71bbe7b12c53f739a056a199b55e4e0b9c372ef0
Salvador Martinez <83838955+mj-salvador@users.noreply.github.com>
2023-05-12 17:14:03 -0600

    first commit

---
+++ WebApplication1/AppConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class AppConfig : IAppConfig
+{
+    public readonly string _testvalue = string.Empty;
+
+    public IConfiguration Configuration { get; }
+    public AppConfig(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        _testvalue = Configuration["TestKey"];
+    }
+
+    public string GetTestValue()
+    {
+        return _testvalue;
+    }
+}
+public interface IAppConfig
+{
+    string GetTestValue();
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IAppConfig appConfig;
+
+        public string TestConfig { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger, IAppConfig appConfig)
+        {
+            _logger = logger;
+            this.appConfig = appConfig;
+            this.TestConfig = this.appConfig.GetTestValue();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
